1894291ab9998ee9a4c76f33227a6a6c7236ff0d
Trevor Drake <trevordrake.gerrit@gmail.com>
2015-03-23 10:07:56 +0000

    mmp : Handle files installed to the rootfs

--- envsetup.sh
+++ envsetup.sh
@@ -6,8 +6,6 @@ function _get_adb_device()
     local TP="product:$( get_build_var TARGET_PRODUCT )"
     local PM="model:$( get_build_var PRODUCT_MODEL  | sed 's/ /_/g' )"
 	local ADB_DEV=$( adb devices -l | grep "$TP" )
-	#echo "A=$ADB_DEV PM=$PM TP=$TP TD=$TD"
-
 	if [ -z "$ADB_DEV" ] ; then
 		ADB_DEV=$( adb devices -l | grep "$PM" )
 	fi
@@ -26,29 +24,31 @@ function mmp()
 		echo "mm failed"
 		return 2;
     fi
-    local ADB_DEV=$(_get_adb_device)
-	if [ -z "$ADB_DEV" ] ; then
+    local LOCAL_ADB_DEV=$(_get_adb_device)
+	if [ -z "$LOCAL_ADB_DEV" ] ; then
 		echo "No Devices Found"
 		return 2;
 	fi
 
-	local ADB_ROOT=$( ANDROID_SERIAL=$ADB_DEV adb shell id | grep -o uid=0 )
-	if [ -z "$ADB_ROOT" ] ; then
+	local LOCAL_ADB_ROOT=$( ANDROID_SERIAL=$LOCAL_ADB_DEV adb shell id | grep -o uid=0 )
+	if [ -z "$LOCAL_ADB_ROOT" ] ; then
 		echo "adb not running as root"
 		return 2;
 	fi
-	local SYS_RW=$( ANDROID_SERIAL=$ADB_DEV adb shell mount | grep /system | grep -o rw )
-	if [ -z "$SYS_RW" ] ; then
+	local LOCAL_SYS_RW=$( ANDROID_SERIAL=$LOCAL_ADB_DEV adb shell mount | grep /system | grep -o rw )
+	if [ -z "$LOCAL_SYS_RW" ] ; then
 		echo "System Partition is readonly"
 		return 2;
 	fi
 
-    local INSTALL_PATHS=$(  mm GET-INSTALL-PATH  | grep INSTALL-PATH: | cut -f3 -d' ' | grep -v $ANDROID_HOST_OUT )
-    for INSTALL_PATH in $INSTALL_PATHS;
+	local LOCAL_TARGET_ROOT_OUT=$(get_build_var TARGET_ROOT_OUT )
+
+    local LOCAL_INSTALL_PATHS=$(  mm GET-INSTALL-PATH  | grep INSTALL-PATH: | cut -f3 -d' ' | grep -v $ANDROID_HOST_OUT )
+    for LOCAL_INSTALL_PATH in $LOCAL_INSTALL_PATHS;
     do
-		PUSH_PATH=$( echo $INSTALL_PATH | sed "s#$ANDROID_PRODUCT_OUT##g" )
-		echo "ANDROID_SERIAL=$ADB_DEV adb push $INSTALL_PATH $PUSH_PATH"
-		ANDROID_SERIAL=$ADB_DEV adb push $INSTALL_PATH $PUSH_PATH
+		LOCAL_PUSH_PATH=$( echo $LOCAL_INSTALL_PATH | sed "s#$LOCAL_TARGET_ROOT_OUT\|$ANDROID_PRODUCT_OUT##g" )
+		echo "ANDROID_SERIAL=$LOCAL_ADB_DEV adb push $LOCAL_INSTALL_PATH $LOCAL_PUSH_PATH"
+		ANDROID_SERIAL=$LOCAL_ADB_DEV adb push $LOCAL_INSTALL_PATH $LOCAL_PUSH_PATH
     done
 
 }
